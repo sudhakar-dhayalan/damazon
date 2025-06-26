573319d0ca366acfd92df584d25402dd3ef9d12e
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2025-06-26 23:57:10 +0530

    feat: logging user

--- src/main/java/com/damazon/backend/controllers/UserController.java
+++ src/main/java/com/damazon/backend/controllers/UserController.java
@@ -3,6 +3,10 @@ package com.damazon.backend.controllers;
 import com.damazon.backend.model.User;
 import com.damazon.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +17,24 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     @PostMapping("register")
     public User registerUser(@RequestBody User user) {
         return userService.registerUser(user);
+    }
+
+    @GetMapping("login")
+    public String login(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword())
+        );
+
+        if (authentication.isAuthenticated()) {
+            return "Success";
+        } else {
+            return "Failure";
+        }
     }
 }
